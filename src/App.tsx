c80f45047043ae6f2f72fa3c2b057d97b08633c2
Sam Watkinson <samwatkinson@outlook.com>
2024-03-24 13:12:53 +0000

    feat: implement initial page UI using data.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css'
 
+import data from '../data.json'
+import { Button } from './components/Button.tsx'
+import { Card } from './components/Card.tsx'
+import { Icon } from './components/Icon.tsx'
+
+// todo: add sorting
+
 function App() {
     return (
         <>
-            <aside></aside>
-            <main></main>
+            <aside>
+                <menu>
+                    <Button icon={<Icon name="arrow-up-a-z" />}>
+                        sort <strong>alphabetically</strong>
+                    </Button>
+                    <Button icon={<Icon name="sterling-sign" />}>
+                        sort by <strong>price</strong>
+                    </Button>
+                    <Button icon={<Icon name="star" />}>
+                        sort by <strong>star rating</strong>
+                    </Button>
+                </menu>
+            </aside>
+            <main>
+                {data.map(item => (
+                    <Card key={item.id} data={item} />
+                ))}
+            </main>
         </>
     )
 }
